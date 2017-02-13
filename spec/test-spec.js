90328e67a41bd40cf19080f41e32d6469ad05b35
incometrader <igdefinition@gmail.com>
2017-02-13 08:39:16 +0100

    initialize as NodeJS app, initialize jasmine and write tests

---
+++ spec/test-spec.js
@@ -0,0 +1,88 @@
+
+(function(){
+  "use strict";
+
+  describe("User Class: Create a user, user can read posts and write comments", function(){
+
+    it("user name and occupation should be a property of the user", function() {
+      var fisayo = new User('Fisayo', 'Entrepreneur');
+      expect(fisayo.name).toBe('Fisayo');
+      expect(fisayo.occupation).toBe('Entrepreneur');
+      expect(fisayo.customProfile).toBe(false);
+    });
+
+    it("user name and occupation should be a property of the user", function() {
+      var linda = new PremiumUser('Linda', 'Blogger', 36, "lindaikeji@gmail.com", ['blogging', 'fashion', 'entertainment']);
+      expect(linda.name).toBe('Linda');
+      expect(linda.occupation).toBe('Blogger');
+      expect(linda.customProfile).toBeTruthy();
+      expect(linda.interests).toBe([blogging, fashion, entertainment]);
+    });
+
+    it("Premium user should be a type of `object`, and an instance of the `User` class", function(){
+      var johnson = new User('Johnson');
+      expect(typeof johnson).toEqual(typeof {});
+      expect(johnson instanceof User).toBeTruthy();
+    });
+
+    it("user has a file storage of 50MB except user is a Developer", function() {
+      var dexter = new User('Dexter', 'Scientist');
+      expect(dexter.fileStorage()).toBe(50);
+
+      var uyiosa = new User('Uyiosa', 'Developer');
+      expect(uyiosa.fileStorage()).toBe(100);
+    
+      expect((function(){return new User('Audax', 'Developer');}()).fileStorage()).toBe(100);
+    });
+
+    it("Premium user has a file storage of 500MB even if Premium user is a Developer", function() {
+      var olamide = new PremiumUser('Olamide', 'Rapper');
+      expect(olamide.fileStorage).toBe(500);
+
+      var iyin = new PremiumUser('Iyinoluwa', 'Developer');
+      expect(iyin.fileStorage).toBe(500);
+    });
+
+    it("user gets no commentLikes until they makes a comment", function() {
+      var victor = new User('Victor', 'Developer');
+      expect(victor.commentLikes).toBe(0);
+      victor.comment(5);
+      expect(victor.commentLikes).toBe(50);
+
+      var tiwa = new User('Tiwatope', 'Nurse');
+      expect(tiwa.commentLikes).toBe(0);
+      tiwa.comment(4);
+      expect(tiwa.commentLikes).toBe(20);
+    });
+
+    it("user gets no point until user reads a post", function() {
+      var daniel = new User('Daniel', 'Banker');
+      expect(daniel.points).toBe(0);
+      daniel.read(3);
+      expect(daniel.points).toBe(15);
+    });
+
+    it("user can integrate 5 apps on our blog except user is less than 16", function() {
+      var junior  = new User('Junior', 'Student', 15);
+      expect(junior.appsIntegrations()).toBe(2);
+
+      var john = new User('John', 'Architect', 24);
+      expect(john.appsIntegrations()).toBe(5);
+    });
+
+    it("user should be called 'Anonymous' if no name is passed as a parameter", function() {
+      var temi = new User();
+      expect(temi.name).toEqual('Anonymous');
+      expect(temi.email).toBe('user@myblog.com');
+    });
+
+    it("user read function should return the instance of the User class", function() {
+      var janet = new User('Janet', 'Trader');
+      var readingTrader = janet.read(7);
+      expect(readingTrader instanceof User).toBeTruthy();
+      expect(typeof readingTrader.read).toBe(typeof (function (){}));
+      expect(janet.points).toBe(readingTrader.points);
+    });
+
+  });
+})();
